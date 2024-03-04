803125ab25b1ad2c13e456d79bed05087b87eaa9
khaledry <157552031+khaledry@users.noreply.github.com>
2024-03-04 12:11:31 +0300

    Update watar0.js

--- watar0.js
+++ watar0.js
@@ -1,3 +1,91 @@
-var _0x2bd1 = [".nav-tabs", "insertBefore", "<form name=\"jump\">\x0D\x0A<div class=\"MyColors\">\x0D\x0A\x09\x09\x09\x09\x09<map id=\"ColorsMap\" name=\"ColorsMap\">\x0D\x0A\x09\x09\x09\x09\x09<area alt=\"\" coords=\"275, 0, 298, 25\" href=\"javascript:chooseStyle(\'none\',60)\" shape=\"rect\" title=\"1\">\x0D\x0A\x09\x09\x09\x09\x09<area alt=\"\" coords=\"244, 0, 266, 25\" href=\"javascript:chooseStyle(\'Co2\',60)\" shape=\"rect\" title=\"2\">\x0D\x0A\x09\x09\x09\x09\x09<area alt=\"\" coords=\"212, 0, 233, 25\" href=\"javascript:chooseStyle(\'Co3\',60)\" shape=\"rect\" title=\"3\">\x0D\x0A\x09\x09\x09\x09\x09<area alt=\"\" coords=\"179, 0, 201, 25\" href=\"javascript:chooseStyle(\'Co4\',60)\" shape=\"rect\" title=\"4\">\x0D\x0A\x09\x09\x09\x09\x09<area alt=\"\" coords=\"148, 0, 170, 25\" href=\"javascript:chooseStyle(\'Co5\',60)\" shape=\"rect\" title=\"5\">\x0D\x0A\x09\x09\x09\x09\x09<area alt=\"\" coords=\"115, 0, 140, 25\" href=\"javascript:chooseStyle(\'Co6\',60)\" shape=\"rect\" title=\"6\">\x0D\x0A\x09\x09\x09\x09\x09<area alt=\"\" coords=\"105, 0, 85, 25\" href=\"javascript:chooseStyle(\'Co7\',60)\" shape=\"rect\" title=\"7\">\x0D\x0A\x09\x09\x09\x09\x09<area alt=\"\" coords=\"75, 0, 52, 25\" href=\"javascript:chooseStyle(\'Co8\',60)\" shape=\"rect\" title=\"8\">\x0D\x0A\x09\x09\x09\x09\x09</map>\x0D\x0A\x09\x09\x09        <img alt=\"\" height=\"40\" src=\"https://1.top4top.net/p_1388gqx1c1.gif\" width=\"300\" usemap=\"#ColorsMap\"></div>\x0D\x0A</form>\x0D\x0A\x0D\x0A\x0D\x0A\x0D\x0A\x0D\x0A\x0D\x0A", "https://cdn.statically.io/gh/waleed-qaid/des/129d2108/www.1des.net-chatt-www.s6acl.com-color1.js", "getScript", "body", "<link rel=\"stylesheet\" type=\"text/css\" href=\"\">\x0D\x0A<link href=\"https://raw.githack.com/waleed-qaid/des3/main/www.1des.net-chatt-www.chaatom.com1.css\" rel=\"stylesheet\" type=\"text/css\" />\x0D\x0A<link href=\"https://raw.githack.com/waleed-qaid/des3/main/www.1des.net-chatt-www.chaatom.com2.css\" media=\"screen\" rel=\"alternate stylesheet\" title=\"Co2\" type=\"text/css\" />\x0D\x0A<link href=\"https://raw.githack.com/khaledry/watar0/main/watar00001.css\" media=\"screen\" rel=\"alternate stylesheet\" title=\"Co3\" type=\"text/css\" />\x0D\x0A<link href=\"https://raw.githack.com/khaledry/watar0/main/watar00002.css\" media=\"screen\" rel=\"alternate stylesheet\" title=\"Co4\" type=\"text/css\" />\x0D\x0A<link href=\"https://raw.githack.com/khaledry/watar0/main/watar00003.css\" media=\"screen\" rel=\"alternate stylesheet\" title=\"Co5\" type=\"text/css\" />\x0D\x0A<style>\x0D\x0A\x0D\x0A\x0D\x0A\x0D\x0A.MyColors {\x0D\x0A    position: absolute;\x0D\x0A    transform: matrix(1, 0, 0, 1, 85, -173);\x0D\x0A    margin-top: 2px;\x0D\x0A    width: 300px;\x0D\x0A    height: 60px;\x0D\x0A    float: left;\x0D\x0A    background: transparent url(https://k.top4top.io/p_2310a8ovj1.png) no-repeat scroll right top;\x0D\x0A    text-align: left;\x0D\x0A}\x0D\x0A\x0D\x0A\x0D\x0A\x0D\x0A\x0D\x0A\x0D\x0A\x0D\x0A\x0D\x0A</style>", ""];
-var _0xe1a4 = [_0x2bd1[0], _0x2bd1[1], _0x2bd1[2], _0x2bd1[3], _0x2bd1[4], _0x2bd1[5], _0x2bd1[6]];
-$(`${_0x2bd1[7]}${_0xe1a4[2]}${_0x2bd1[7]}`)[_0xe1a4[1]](_0xe1a4[0]), $[_0xe1a4[4]](_0xe1a4[3]), $(`${_0x2bd1[7]}${_0xe1a4[6]}${_0x2bd1[7]}`)[_0xe1a4[1]](_0xe1a4[5])
+var manual_or_random = 'manual';
+var randomsetting = '3 days';
+
+function getCookie(_0x2915x4) {
+    var _0x2915x5 = new RegExp(_0x2915x4 + '=[^;]+', 'i');
+    if (document.cookie.match(_0x2915x5)) {
+        return document.cookie.match(_0x2915x5)[0].split('=')[1]
+    };
+    return null
+}
+
+function setCookie(_0x2915x7, _0x2915x8, _0x2915x9) {
+    var _0x2915xa = new Date();
+    var _0x2915xb = (typeof _0x2915x9 != 'undefined') ? _0x2915xa.setDate(_0x2915xa.getDate() + parseInt(_0x2915x9)) : _0x2915xa.setDate(_0x2915xa.getDate() - 5);
+    document.cookie = _0x2915x7 + '=' + _0x2915x8 + '; expires=' + _0x2915xa.toGMTString() + '; path=/'
+}
+
+function deleteCookie(_0x2915x7) {
+    setCookie(_0x2915x7, 'moot')
+}
+
+function setStylesheet(_0x2915xe, _0x2915xf) {
+    var _0x2915x10, _0x2915x11, _0x2915x12 = [''];
+    for (_0x2915x10 = 0;
+        (_0x2915x11 = document.getElementsByTagName('link')[_0x2915x10]); _0x2915x10++) {
+        if (_0x2915x11.getAttribute('rel').toLowerCase() == 'alternate stylesheet' && _0x2915x11.getAttribute('title')) {
+            _0x2915x11.disabled = true;
+            _0x2915x12.push(_0x2915x11);
+            if (_0x2915x11.getAttribute('title') == _0x2915xe) {
+                _0x2915x11.disabled = false
+            }
+        }
+    };
+    if (typeof _0x2915xf != 'undefined') {
+        var _0x2915x13 = Math.floor(Math.random() * _0x2915x12.length);
+        _0x2915x12[_0x2915x13].disabled = false
+    };
+    return (typeof _0x2915xf != 'undefined' && _0x2915x12[_0x2915x13] != '') ? _0x2915x12[_0x2915x13].getAttribute('title') : ''
+}
+
+function chooseStyle(_0x2915x15, _0x2915x9) {
+    if (document.getElementById) {
+        setStylesheet(_0x2915x15);
+        setCookie('mysheet', _0x2915x15, _0x2915x9)
+    }
+}
+
+function indicateSelected(_0x2915x17) {
+    if (selectedtitle != null && (_0x2915x17.type == undefined || _0x2915x17.type == 'select-one')) {
+        var _0x2915x17 = (_0x2915x17.type == 'select-one') ? _0x2915x17.options : _0x2915x17;
+        for (var _0x2915x10 = 0; _0x2915x10 < _0x2915x17.length; _0x2915x10++) {
+            if (_0x2915x17[_0x2915x10].value == selectedtitle) {
+                if (_0x2915x17[_0x2915x10].tagName == 'OPTION') {
+                    _0x2915x17[_0x2915x10].selected = true
+                } else {
+                    _0x2915x17[_0x2915x10].checked = true
+                };
+                break
+            }
+        }
+    }
+}
+if (manual_or_random == 'manual') {
+    var selectedtitle = getCookie('mysheet');
+    if (document.getElementById && selectedtitle != null) {
+        setStylesheet(selectedtitle)
+    }
+} else {
+    if (manual_or_random == 'random') {
+        if (randomsetting == 'eachtime') {
+            setStylesheet('', 'random')
+        } else {
+            if (randomsetting == 'sessiononly') {
+                if (getCookie('mysheet_s') == null) {
+                    document.cookie = 'mysheet_s=' + setStylesheet('', 'random') + '; path=/'
+                } else {
+                    setStylesheet(getCookie('mysheet_s'))
+                }
+            } else {
+                if (randomsetting.search(/^[1-9]+ days/i) != -1) {
+                    if (getCookie('mysheet_r') == null || parseInt(getCookie('mysheet_r_days')) != parseInt(randomsetting)) {
+                        setCookie('mysheet_r', setStylesheet('', 'random'), parseInt(randomsetting));
+                        setCookie('mysheet_r_days', randomsetting, parseInt(randomsetting))
+                    } else {
+                        setStylesheet(getCookie('mysheet_r'))
+                    }
+                }
+            }
+        }
+    }
+}
